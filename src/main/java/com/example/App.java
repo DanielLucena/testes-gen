a1e9cf4bffd632da1e53ea978cf1eb85656c3b50
Daniel Lucena <drlucena@gmail.com>
2024-10-13 13:59:30 +0000

    feat: Implementa exemplo de uso das classes Banco e Conta no metodo main

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,14 +4,71 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Banco bancoDoBrasil = new Banco(1, "Banco do Brasil");
 
-        Conta conta = new Conta();
-        char digito = Validador.calculaDigitoVerificador(262634);
-        System.out.println(digito);
+        Conta contaFulano = new Conta("Fulano de Tal", "123456");
+        Conta contaBeltrano = new Conta("Beltrano de não sei das quantas", "141516");
+
+        bancoDoBrasil.addConta(contaFulano);
+        bancoDoBrasil.addConta(contaBeltrano);
+
+        System.out.println("saldo beltrano: " + contaBeltrano.getSaldo());
+        bancoDoBrasil.depositar(contaBeltrano, 500.00, "141516");
+        System.out.println("saldo beltrano: " + contaBeltrano.getSaldo());
+
+        // char digito = Validador.calculaDigitoVerificador(262634);
+        System.out.println("quantidade de contas: " + bancoDoBrasil.getContas().size());
+
+        try {
+            Conta contaSicrano = new Conta("Sicrano", "123456789");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Conta contaSicrano = new Conta("Sicrano", "1234");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Conta contaSicrano = new Conta("Sicrano", "558764");
+
+        try {
+            bancoDoBrasil.transferencia(contaBeltrano, contaSicrano, 5.50, "141516");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        bancoDoBrasil.addConta(contaSicrano);
+
+        try {
+            bancoDoBrasil.transferencia(contaBeltrano, contaSicrano, 5.50, "141516");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Runnable[] operations = {
+                () -> bancoDoBrasil.depositar(contaSicrano, 1000.0, "558764"),
+                () -> bancoDoBrasil.sacar(contaFulano, 1.0, "123456"),
+                () -> bancoDoBrasil.sacar(contaSicrano, 500.51, "558763"),
+                () -> bancoDoBrasil.transferencia(contaBeltrano, contaFulano, 5.50, "141516"),
+
+        };
+
+        for (Runnable operation : operations) {
+            try {
+                operation.run();
+                System.out.println(
+                        "Sucesso em operação " + operation.hashCode());
+            } catch (RuntimeException e) {
+                System.out.println(
+                        "Falha   em operação " + operation.hashCode() + " [" + e.getMessage() + "] ");
+
+            }
+
+        }
     }
 }
--- src/main/java/com/example/ValidadtionErrorException.java
+++ src/main/java/com/example/ValidadtionErrorException.java
@@ -3,7 +3,7 @@ package com.example;
 public class ValidadtionErrorException extends RuntimeException {
 
     ValidadtionErrorException(String validacao) {
-        super("Erro de Validação em : " + validacao);
+        super("Erro de Validação: " + validacao);
     }
 
 }
