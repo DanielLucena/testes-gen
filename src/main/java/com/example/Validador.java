2d41bf9d8885cf7a12e805ccdb45225f815189d4
Daniel Lucena <drlucena@gmail.com>
2024-10-13 13:49:28 +0000

    feat: adiciona geração por hash de numero da conta e validação de acesso a conta por senha
    tambem adiciona validações de conta ativa, criação de senha e saldo suficiente

--- src/main/java/com/example/Validador.java
+++ src/main/java/com/example/Validador.java
@@ -1,11 +1,24 @@
 package com.example;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Validador {
 
     public static int incremental = 0;
+
+    public static MessageDigest digest;
+
+    static {
+        try {
+            // Cria uma instância do algoritmo de hash SHA-256
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * Says hello to the world.
@@ -46,5 +59,53 @@ public class Validador {
         return incremental;
     }
 
+    public static void validaIsContaAtiva(Conta conta) {
+        if (!conta.isAtiva()) {
+            throw new ValidadtionErrorException("essa conta nõo está ativa");
+        }
+    }
 
+    public static void validaCriacaoSenha(String senha) {
+        if (!senha.matches("\\d+")) {
+            throw new ValidadtionErrorException("a senha deve conter apenas numeros");
+        }
+        if (senha.length() != 6) {
+            throw new ValidadtionErrorException("a senha deve ter exatamente 6 numeros");
+        }
+    }
+
+    public static int geraNumeroConta(String beneficiario, String senha, long id) {
+
+        // Concatenar as entradas
+        String concatenatedInput = beneficiario + senha + id;
+
+        // Aplica o algoritmo de hash ao input concatenado
+        byte[] hashBytes = digest.digest(concatenatedInput.getBytes());
+
+        // Converter os primeiros bytes do hash para um valor numérico
+        long hashValue = 0;
+        for (int i = 0; i < 4; i++) { // Usar os 4 primeiros bytes para gerar um número
+            hashValue = (hashValue << 8) + (hashBytes[i] & 0xFF);
+        }
+
+        // Garantir que o valor esteja entre 10.000.000 e 90.000.000
+        int min = 0;
+        int max = 90000000;
+        int result = (int) (min + (hashValue % (max - min)));
+
+        return result;
+    }
+
+    public static void validaSenhaConta(Conta conta, String senha) {
+        int numeroContaCalculado = geraNumeroConta(conta.getBeneficiario(), senha, conta.getId().longValue());
+        if (conta.getNumeroConta() != numeroContaCalculado) {
+            throw new ValidadtionErrorException("senha incorreta");
+        }
+    }
+
+    public static void validarSaldoSuficiente(Conta conta, Double valor) {
+        if (valor.doubleValue() > conta.getSaldo().doubleValue()) {
+            throw new ValidadtionErrorException("saldo insuficiente");
+        }
+    }
 }
